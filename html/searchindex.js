7254f99e5c3ad835b6b21dbebf500020c7f34253
apatniv <apatniv@gmail.com>
2017-05-22 08:51:31 -0400

    removed unnecessary files

--- html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:48,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to test_github_hosting&#8217;s documentation!"],titleterms:{document:0,indic:0,tabl:0,test_github_host:0,welcom:0}})
